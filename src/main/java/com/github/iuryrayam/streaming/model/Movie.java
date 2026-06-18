8afa2b25710f4d1329a2b3d9914810a87652f9b2
IuryRayam <iury2155@gmail.com>
2026-06-18 15:15:18 -0300

    fix Movie

--- src/main/java/com/github/iuryrayam/streaming/model/Movie.java
+++ src/main/java/com/github/iuryrayam/streaming/model/Movie.java
@@ -1,4 +1,46 @@
 package com.github.iuryrayam.streaming.model;
 
+import com.github.iuryrayam.streaming.model.enums.GenderMovie;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "movie")
+@Data
 public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "title", length = 150, nullable = false)
+    private String title;
+
+    @Column(name = "release_date", nullable = false)
+    private LocalDate releaseDate;
+
+    @Column(name = "duration", nullable = false)
+    private Integer duration;
+
+    @Column(name = "rating", nullable = false)
+    private Integer rating;
+
+    @Column(name = "studio", length = 150, nullable = false)
+    private String studio;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "gender", length = 30, nullable = false)
+    private GenderMovie gender;
+
+    @Column(name = "price", precision = 18, scale = 2)
+    private BigDecimal price;
+
+    @ManyToOne
+    @JoinColumn(name = "id_director", nullable = false)
+    private Director director;
 }
+
