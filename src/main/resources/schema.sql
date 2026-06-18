6d6b7e45795eded01099d050ff9747dfe53b3e86
IuryRayam <iury2155@gmail.com>
2026-06-18 14:22:50 -0300

    first commit

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,20 @@
+create table director(
+    id uuid not null primary key,
+    name varchar(100) not null,
+    date_of_birth date not null,
+    nationality varchar(50) not null
+);
+
+create table movie(
+    id uuid not null primary key,
+    title varchar(150) not null,
+    release_date date not null,
+    duration int not null,
+    rating int not null,
+    studio varchar(150) not null,
+    gender varchar(30) not null,
+    price numeric(10, 2),
+    id_director uuid not null references director(id),
+
+    constraint chk_gender check (gender in ('FICTION', 'FANTASY', 'MYSTERY', 'ROMANCE', 'DRAMA'))
+);
