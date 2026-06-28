d8e4325617abd2cf8e372ca6d1c94e87be1557f8
IuryRayam <iury2155@gmail.com>
2026-06-28 14:05:54 -0300

    adding more validations in DirectorDTO

--- src/main/java/com/github/iuryrayam/streaming/controller/dto/DirectorDTO.java
+++ src/main/java/com/github/iuryrayam/streaming/controller/dto/DirectorDTO.java
@@ -3,6 +3,8 @@ package com.github.iuryrayam.streaming.controller.dto;
 import com.github.iuryrayam.streaming.model.Director;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 import java.util.UUID;
@@ -10,10 +12,13 @@ import java.util.UUID;
 public record DirectorDTO(
         UUID id,
         @NotBlank(message = "campo obrigatorio")
+        @Size(min = 2, max = 150, message = "campo fora do tamanho padrão")
         String name,
         @NotNull(message = "campo obrigatorio")
+        @Past(message = "Não pode ser uma data futura")
         LocalDate dateBirth,
         @NotBlank(message = "campo obrigatorio")
+        @Size(min = 2, max = 50, message = "campo fora do tamanho padrão")
         String nationality
 ) {
 
