baa87ca97df24529532f1105db2e2a3f882c7800
IuryRayam <iury2155@gmail.com>
2026-06-29 11:27:45 -0300

    adding validation in RequestMovie

--- src/main/java/com/github/iuryrayam/streaming/controller/dto/RequestMovie.java
+++ src/main/java/com/github/iuryrayam/streaming/controller/dto/RequestMovie.java
@@ -1,19 +1,29 @@
 package com.github.iuryrayam.streaming.controller.dto;
 
 import com.github.iuryrayam.streaming.model.enums.GenderMovie;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
 public record RequestMovie(
+        @NotBlank(message = "campo obrigatorio")
         String title,
+        @NotNull(message = "campo obrigatorio")
+        @Past(message = "não pode ser uma data futura")
         LocalDate releaseDate,
+        @NotNull(message = "campo obrigatorio")
         Integer duration,
+        @NotNull(message = "campo obrigatorio")
         Integer rating,
+        @NotBlank(message = "campo obrigatorio")
         String studio,
         GenderMovie gender,
         BigDecimal price,
+        @NotNull(message = "campo obrigatorio")
         UUID idDirector
 ) {
 }
